45f774f1a139b0bd4cba540f90fee98a3d01dc7c
honey <honey.arora@tothenew.com>
2022-03-07 18:57:45 +0530

    QUES1

--- src/main/java/com/example/Restwebservices/WelcomeController.java
+++ src/main/java/com/example/Restwebservices/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.example.Restwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     //we want get method to return welcome with uri=/welcome
     //map get request with uri
-    @RequestMapping(method = RequestMethod.GET,path = "/welcome")
+    @GetMapping(path = "/welcome")
     public String welcome(){
         return "WELCOME TO SPRING BOOT";
     }
